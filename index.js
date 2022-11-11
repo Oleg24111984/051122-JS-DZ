984de38b80f4f5ff2b9979e56f53cb45cb23d567
Your Name <you@example.com>
2022-11-11 21:47:29 +0300

    Созданы index.js, index.html

---
+++ index.js
@@ -0,0 +1,37 @@
+let a = 10;
+let b = 12;
+let c = 5;
+if ((a > b) && (a > c)) {
+    console.log (a);
+} else if ((b > a) && (b > c)) {
+    console.log (b);
+}
+else if ((c > a) && (c > b)) {
+    console.log (c);
+}
+
+let t = 37;
+if (t < -30) {
+    console.log ('Оставайтесь дома!');
+} else if ((t >= -30) && (t <= -10)) {
+    console.log ('Сегодня холодно');
+}
+else if ((t > -10) && (t <= 5)) {
+    console.log ('Не холодно');
+}
+else if ((t > 5) && (t <= 15)) {
+    console.log ('Тепло');
+}
+else if ((t > 15) && (t <= 25)) {
+    console.log ('Очень тепло');
+}
+else if ((t > 25) && (t < 35)) {
+    console.log ('Жарко');
+}
+else if ((t >= 35)) {
+    console.log ('Пекло!');
+}
+
+
+
+
